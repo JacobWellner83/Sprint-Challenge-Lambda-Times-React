4fc203c80368af18c2334b05b549eb0527dbcda1
JacobWellner83 <jcwellner83@gmail.com>
2019-06-01 11:54:22 -0500

    Carousel successfully configured

--- lambdatimes/src/components/Carousel/Carousel.js
+++ lambdatimes/src/components/Carousel/Carousel.js
@@ -14,21 +14,38 @@ export default class Carousel extends Component {
   }
 
   leftClick = () => {
-
+    this.setState(state => {
+      return {
+        selectedImageIndex: state.selectedImageIndex === 0
+          ? state.carouselItems.length - 1
+          : --state.selectedImageIndex
+      }
+    })
   }
 
   rightClick = () => {
-
+    this.setState(state => {
+      return {
+        selectedImageIndex: state.selectedImageIndex === state.carouselItems.length - 1
+          ? 0
+          : ++state.selectedImageIndex
+      }
+    })
   }
 
   selectedImage = () => {
-    return <img src={} style={{display: 'block'}} />
+    const { carouselItems, selectedImageIndex } = this.state
+    return <img
+      src={carouselItems[selectedImageIndex]}
+      alt="Description not available"
+      style={{display: 'block'}} />
   }
   
   render(){
     return (
       <div className="carousel">
         <div className="left-button" onClick={this.leftClick}>{"<"}</div>
+        {this.selectedImage()}
         <div className="right-button" onClick={this.rightClick}>{">"}</div>
       </div>
     )
